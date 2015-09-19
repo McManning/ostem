0f08b29807bedb1f5fd8a510371bf9310302c962
Chase McManning <cmcmanning@gmail.com>
2015-09-19 16:15:54 -0400

    Adding basic framework files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+$(function() {
+    $('.collapsible').collapsible({
+        accordion : false
+    });
+
+    $('.scrollspy').scrollSpy();
+    
+    $('.parallax').parallax();
+
+    $(".button-collapse").sideNav({
+        menuWidth: 300,
+        edge: 'left'
+    });
+
+});
+
